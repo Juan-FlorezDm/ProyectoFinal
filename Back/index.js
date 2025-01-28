d23a54fd967f1a9b709d51232b545f5225b5c901
Juan-FlorezDm <jnendez38@gmail.com>
2025-01-28 18:36:22 -0500

    añadiendo funcionalidades y parte del front

--- Back/index.js
+++ Back/index.js
@@ -1,27 +1,34 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
+
+const path = require("path")
+
 const routerEstudiantes = require("./routes/estudiante.js")
+const FrontPath = path.resolve(__dirname, "../Front/HTML/")
+
 
 const app = express()
 const port = 3307
 
 app.set("port", port)
 
-mongoose.connect(process.env.MONGO_DB_URL)
+mongoose.connect("mongodb+srv://jnendez38:BLPWiCGyAWRXbFvR@basedatostalentotech.zli5t.mongodb.net/")
     .then(()=>{
         console.log("Conectado a la base de datos")
     })
-    .catch((err)=>{
+    .catch((err)=>{ 
         console.error(err.message)
     })
 
 
 app.use(express.json())
 app.use("/api/estudiante", routerEstudiantes)
+app.use(express.static(FrontPath))
+
 
 app.get("/", (req,res)=>{
-    res.send("Encendido")
+    res.sendFile(path.join(FrontPath, 'DashBoard.html'))
 })
 
 app.listen(port, ()=>{
